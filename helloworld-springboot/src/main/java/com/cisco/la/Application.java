f8cca8576681265805f60932b87ec2ea1890b6a2
javaage <subing.xiao@pactera.com>
2018-01-25 11:28:05 +0800

    add predix

--- helloworld-springboot/src/main/java/com/cisco/la/Application.java
+++ helloworld-springboot/src/main/java/com/cisco/la/Application.java
@@ -19,12 +19,19 @@ package com.cisco.la;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 import org.springframework.web.bind.annotation.RestController;
 
 
+@EnableAutoConfiguration
+@ComponentScan(basePackages = "com.cisco.la")
+@EnableCaching
+@EnableTransactionManagement
 @SpringBootApplication
-@RestController
 public class Application {
 	public final static Logger logger = LoggerFactory.getLogger(Application.class);
 	
